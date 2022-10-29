50861dc62ed090cbe695feb1ea80f1f26b565336
WALL.E <1770957624@qq.com>
2022-10-29 17:02:06 +0800

    Selectable animate

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <script lang="ts" setup>
 
 // import "./assets/style.css";
-import { onMounted } from "vue";
+import { onMounted, reactive, Ref, ref } from "vue";
 import dog_img from "./assets/dog.png";
 
-let canvas: HTMLCanvasElement;
 
 // Init canvas
-canvas = document.querySelector("canvas") ?? document.createElement("canvas");
-canvas.id = "sprit"
-const app = document.querySelector("#app")
-app?.appendChild(canvas);
+const canvas =
+  document.querySelector("canvas") ?? document.createElement("canvas");
+onMounted(() => {
+  canvas.id = "sprit"
+  const app = document.querySelector("#app")
+  app?.appendChild(canvas);
+})
 
 const ctx = canvas.getContext("2d");
 const WIDTH = (canvas.width = 600);
 const HEIGHT = (canvas.height = 600);
 
 // Control the sprit option
-const framesX = [
+const Frames = [
   { name: "idle", frames: 7 },
   { name: 'jump', frames: 7 },
   { name: 'down', frames: 7 },
@@ -29,18 +31,18 @@ const framesX = [
   { name: 'ko', frames: 12 },
   { name: 'getHit', frames: 4 }] as const
 
-type animateName = (typeof framesX)[number]["name"]
+type animateName = (typeof Frames)[number]["name"]
 const Sprit = {
   width: 6900 / 12,
   height: 5230 / 10,
   frameX: 0,
   frameY: 0,
-  framesX,
-  staggerFrame: 2,
+  Frames,
+  staggerFrame: 3,
   gameFrame: 0,
   currentFames(name: animateName) {
     let { frames } =
-      this.framesX.find((item, index) => {
+      this.Frames.find((item, index) => {
         this.frameY = index;
         return item["name"] == name
       }) as { name: string, frames: number }
@@ -53,14 +55,16 @@ const Sprit = {
 const dogAnimate = new Image();
 dogAnimate.src = dog_img;
 
+let selected = ref("run") as Ref<animateName>
 function animate() {
   ctx?.clearRect(0, 0, WIDTH, HEIGHT);
-
+  let position = Math.floor(Sprit.gameFrame / Sprit.staggerFrame) % Sprit.currentFames(selected.value);
+  Sprit.frameX = Sprit.width * position;
   // (IMAGE,dx,dy,dw,dh,cx,cy,cw,ch)
   // dx 控制裁剪的位置大小，cx 控制显示区域的位置大小
   ctx?.drawImage(
     dogAnimate,
-    Sprit.width * Sprit.frameX,
+    Sprit.frameX,
     Sprit.height * Sprit.frameY,
     Sprit.width,
     Sprit.height,
@@ -70,11 +74,7 @@ function animate() {
     Sprit.height
   );
 
-  if (Sprit.frameX < Sprit.currentFames("jump") - 1) {
-    Sprit.frameX++
-  } else { Sprit.frameX = 0 }
-
-  // Sprit.gameFrame++
+  Sprit.gameFrame++
   requestAnimationFrame(animate);
 }
 
@@ -82,10 +82,27 @@ animate();
 
 </script>
 <template >
-
+  <select name="animation" id="animate-select" v-model="selected">
+    <option v-for="item in Frames" :value="item.name">{{ item.name }}</option>
+  </select>
 </template>
+
 <style>
 #sprit {
+  display: block;
+  width: 80vw;
+  height: 80vw;
   border: 2px solid rgb(229, 229, 229);
+}
+
+#animate-select {
+  width: 70px;
+  height: 25px;
+  margin-bottom: 25px;
+  font-size: medium;
+}
+
+#animate-select option {
+  text-align: center;
 }
 </style>
